90f05ed16a68c73165aaa4a4066545c5697172de
Filip <151084737+qFilipo@users.noreply.github.com>
2023-12-30 17:11:25 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,26 @@
+changeImg()
+generateAdvise()
+
+let advice = new Object;
+async function generateAdvise(){
+    await fetch("https://api.adviceslip.com/advice")
+    .then(response => {
+        if(!response.ok){
+            throw new Error("Network response was not ok!")
+        }
+        return response.json();
+    })
+    .then(data=>{
+       advice = data;
+       console.log(advice.slip.advice)
+    })
+    .catch(error => {
+        console.error('There has been a problem with your fetch operation:', error);
+    });
+}
+
+console.log(advice)
+
 function changeImg(){
     const imgSrc = document.getElementById("divider")
     const changeSrc = () => {
@@ -13,4 +36,3 @@ function changeImg(){
     changeSrc();
 }
 
-changeImg()
