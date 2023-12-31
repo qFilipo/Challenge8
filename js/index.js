22b2a8bcda3941bac395c867ae330348e239a97b
Filip <151084737+qFilipo@users.noreply.github.com>
2023-12-31 12:38:42 +0100

    update

--- js/index.js
+++ js/index.js
@@ -1,26 +1,40 @@
-changeImg()
-generateAdvise()
+let advice = new Array;
+let i=0;
+let firstLog = true;
 
-let advice = new Object;
-async function generateAdvise(){
-    await fetch("https://api.adviceslip.com/advice")
-    .then(response => {
-        if(!response.ok){
-            throw new Error("Network response was not ok!")
-        }
-        return response.json();
-    })
-    .then(data=>{
-       advice = data;
-       console.log(advice.slip.id)
-       console.log(advice.slip.advice)
-    })
-    .catch(error => {
-        console.error('There has been a problem with your fetch operation:', error);
-    });
+const btn=document.getElementById("icon-dice-container")
+btn.style.background = "grey"
+btn.style.boxShadow = "0 0 0 grey";
+
+setInterval(function(){
+    if(advice.length <=10){
+        generateAdvice()
+    }
+}, 5000)
+
+function generateAdvice(){
+    fetch("https://api.adviceslip.com/advice")
+        .then(response => {
+            if(!response.ok){
+                throw new Error("Network response was not ok!")
+            }
+            return response.json();
+        })
+        .then(data=>{
+                advice.push(data);
+                if (firstLog===true){
+                    checkAdvice()
+                    firstLog = false
+                    setTimeout(function(){
+                        btn.style.background = ""
+                        btn.style.boxShadow = "";
+                    },5000)
+                }
+        })
+        .catch(error => {
+            console.error('There has been a problem with your fetch operation:', error);
+        });
 }
-
-console.log(advice)
 
 function changeImg(){
     const imgSrc = document.getElementById("divider")
@@ -33,7 +47,44 @@ function changeImg(){
         }
     }
     window.addEventListener("resize", changeSrc);
-
     changeSrc();
 }
 
+function btnAdvice(){
+    let isClickable = true
+    btn.addEventListener("click", ()=> {
+        if (isClickable){
+
+            isClickable = false
+            btn.style.background = "grey"
+            btn.style.boxShadow = "0 0 0 grey";
+
+            checkAdvice()
+
+            advice.shift()
+            console.log(advice)
+
+            setTimeout(function(){
+                btn.style.background = ""
+                btn.style.boxShadow = "";
+                isClickable = true
+            }, 5000)
+        }
+    })
+}      
+
+function checkAdvice(){
+    const textAdvice=document.getElementById("advise-text")
+    const idAdvice=document.getElementById("number")
+    if(textAdvice.textContent != '"'+advice[0].slip.advice+'"'){
+        textAdvice.textContent = '"'+advice[0].slip.advice+'"'
+        idAdvice.textContent = advice[0].slip.id
+    }
+    else{
+        advice.shift()
+        checkAdvice()
+    }
+}
+
+changeImg()
+btnAdvice()
